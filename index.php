1e662279efc36b28483e2c113dbf5d557f09b360
Ryan <ryan.elr@outlook.com>
2018-07-25 14:26:09 +0200

    ex2

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice2 Part1</title>
+  </head>
+  <body>
+    <?php
+    $firstname = 'El-rhichachmi';
+    $lastname = 'Ryan';
+    $age = 22;
+    echo $firstname.'<br/>';
+    echo $lastname.'<br/>';
+    echo $age;
+    ?>
+  </body>
+</html>
